5a717b152e0a639ea8294686c4442d221964cbcf
FTAMA <fatma.dhibi87@gmail.com>
2019-12-03 16:41:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.distoration'),
+    intensity: 0.9,
+    image1: 'https://images.pexels.com/photos/2020991/pexels-photo-2020991.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+   
+   
+    image2: 'https://images.pexels.com/photos/2020992/pexels-photo-2020992.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    angle: Math.PI /8,
+    speedIn: 2,
+    speedOut: 5,
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/heightMap.png',
+})
